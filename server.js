2200fde5aa3aedb58fbe9ea376206034a8074066
Uros Zivaljevic <uroszivaljevic19@gmail.com>
2017-10-17 23:50:29 +0200

    add server.js and fix start scripts

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by uroszivaljevic on 10/17/17.
+ */
+
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + "/dist/app/"));
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname + "/dist/app/", "index.html"));
+// });
+
+app.listen(port, function () {
+    console.log("SERVER START");
+});

2a88ebaa11131f4d6df85d1a90f017f25338fe46
Uros Zivaljevic <uroszivaljevic19@gmail.com>
2017-11-19 19:22:55 +0100

    add cors

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.static(__dirname + "/dist/app/"));
 
 // app.get("*", (req, res) => {
